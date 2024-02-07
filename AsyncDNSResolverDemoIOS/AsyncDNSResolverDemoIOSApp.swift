08ced57ed50ba4b1b67b5813a98e28cf02164c57
Alex <alex@lpdev.pro>
2024-02-07 11:10:27 -0300

    Initial Commit

---
+++ AsyncDNSResolverDemoIOS/AsyncDNSResolverDemoIOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  AsyncDNSResolverDemoIOSApp.swift
+//  AsyncDNSResolverDemoIOS
+//
+//  Created by Alex on 07/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct AsyncDNSResolverDemoIOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
